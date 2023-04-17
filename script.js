00648107d2aebeedc420b67b6309b7d9e3e897eb
Rhys Chorlton <rhysca99@gmail.com>
2023-04-17 17:04:32 +0100

    Added functionality to allow user to input a game into the library array

---
+++ script.js
@@ -0,0 +1,42 @@
+const addGameButton = document.querySelector('.add-button');
+
+const myGameLibrary = [];
+
+class Game {
+  constructor(gameName, gameGenre, gamePlayed, gameTimePlayed) {
+    this.gameName = gameName;
+    this.gameGenre = gameGenre;
+    this.gamePlayed = gamePlayed;
+    this.gameTimePlayed = gameTimePlayed;
+  }
+}
+
+function addGameToLibrary(gameName, gameGenre, gamePlayed, gameTimePlayed) {
+  const game = new Game(gameName, gameGenre, gamePlayed, gameTimePlayed);
+  myGameLibrary.push(game);
+}
+
+function validation() {
+  const form = document.querySelector('form');
+  const gameNameInput = document.querySelector('#game-name');
+  const gameGenreInput = document.querySelector('#game-genre');
+  const checkbox = document.querySelector('input[name="checkbox"]');
+  const gameTimePlayedInput = document.querySelector('#game-time-played');
+
+  if (
+    gameNameInput.value !== '' &&
+    gameGenreInput.value !== '' &&
+    gameTimePlayedInput.value !== ''
+  ) {
+    const gamePlayed = checkbox.checked;
+    addGameToLibrary(
+      gameNameInput.value,
+      gameGenreInput.value,
+      gamePlayed,
+      gameTimePlayedInput.value
+    );
+    form.reset();
+  }
+}
+
+addGameButton.addEventListener('click', validation);
